41a102832c63c42099474c2ec9641700b0ef59a0
Tanmay Raj <118921083+tanmayraj2309@users.noreply.github.com>
2024-01-19 18:56:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// let ctitle = document.getElementById("firstcard")
+
+
+let ctitle = document.getElementsByClassName("card-title")
+
+
+ctitle[0].style.color = "red";
+ctitle[1].style.color = "green";
+ctitle[2].style.color = "blue";
+ctitle[3].style.color = "#4C0027";
+ctitle[4].style.color = "red";
+ctitle[5].style.color = "green";
+ctitle[6].style.color = "blue";
+ctitle[7].style.color = "#4C0027";
+ctitle[8].style.color = "red";
+ctitle[9].style.color = "green";
+ctitle[10].style.color = "blue";
+ctitle[11].style.color = "#4C0027";
+
+
+// document.querySelector(".card-text").style.background = "red";
+
